bce131b8aeaf76db693bb77764479b16378d7ffa
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-12 12:55:27 -0800

    Test good fail (return Class Calculator).

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,5 +1,9 @@
-import Calculator from './src/js/calculator.js';
+import { TestScheduler } from 'jest';
+import Calculator from './../src/js/calculator.js';
 
 describe("Calculator", () => {
 
+    test('should return the Class Calculator', () => {
+        expect(Calculator).toEqual();
+    });
 });
