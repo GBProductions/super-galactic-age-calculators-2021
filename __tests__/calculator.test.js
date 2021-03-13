aa6bbe5dcbcad28ea0d297afe53b72024f9c02a3
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-13 14:36:13 -0800

    pass beforeEach() test.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,7 +1,16 @@
-import { TestScheduler } from 'jest';
 import Calculator from './../src/js/calculator.js';
 
 describe("Calculator", () => {
+    let calculator;
+
+
+    beforeEach(() => {
+        calculator = new Calculator ("test");
+    });
+
+    test('should show how beforeEach() works', () => {
+        console.log(calculator);
+    });
 
     test('should return the Class Calculator', () => {
         let calculator = new Calculator ("test")
