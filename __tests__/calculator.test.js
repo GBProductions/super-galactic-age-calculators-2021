c1ae61e2dcbccac6cb85ddcb3c7beef21a1f5057
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-13 18:16:01 -0800

    pass Life Left test Mercury.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -24,7 +24,7 @@ describe("Calculator", () => {
 
     test('should return users life left on Mercury', () => {
         let calculator = new Calculator (100, "Mercury", 200);
-        expect(calculator.planetAge()).toEqual();
+        expect(calculator.planetAge()).toEqual(42);
     });
 
     
