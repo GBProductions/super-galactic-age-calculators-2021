3a936a0c085e50052b310d4de042faf01c9f2c86
Garrett Brown <garrettpaulbrown@gmail.com>
2021-03-26 17:14:05 -0700

    Change syntax

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,33 +4,32 @@ export default class Calculator {
         this.planet = planet;
         this.avgEarthYear = avgEarthYear;
     }
-}
 
-Calculator.prototype.planetAge = function() {
-    if (this.planet === "Mercury") {
-        return (this.earthYear * 0.42) 
-    }   else if (this.planet === "Venus") {
-        return (this.earthYear * 0.62)
-    }   else if (this.planet === "Mars") {
-        return (this.earthYear * 1.88)
-    }   else if (this.planet === "Jupiter") {
-        return (this.earthYear * 11.86)
+    planetAge() {
+        if (this.planet === "Mercury") {
+            return (this.earthYear * 0.42) 
+        }   else if (this.planet === "Venus") {
+            return (this.earthYear * 0.62)
+        }   else if (this.planet === "Mars") {
+            return (this.earthYear * 1.88)
+        }   else if (this.planet === "Jupiter") {
+            return (this.earthYear * 11.86)
+        }
     }
-}
 
-Calculator.prototype.lifeLeft = function() {
-
-    if (this.planet === "Mercury") {
-        let timePast = ((this.avgEarthYear - this.earthYear) * 0.42); 
-        return timePast 
-    }   else if (this.planet === "Venus") {
-        let timePast = ((this.avgEarthYear - this.earthYear) * 0.62)
-        return timePast
-    }   else if (this.planet === "Mars") {
-        let timePast = ((this.avgEarthYear - this.earthYear) * 1.88)
-        return timePast
-    }   else if (this.planet === "Jupiter") {
-        let timePast = ((this.avgEarthYear - this.earthYear) * 11.86)
-        return timePast
-    }   
+    lifeLeft() {
+        if (this.planet === "Mercury") {
+            let timePast = ((this.avgEarthYear - this.earthYear) * 0.42); 
+            return timePast 
+        }   else if (this.planet === "Venus") {
+            let timePast = ((this.avgEarthYear - this.earthYear) * 0.62)
+            return timePast
+        }   else if (this.planet === "Mars") {
+            let timePast = ((this.avgEarthYear - this.earthYear) * 1.88)
+            return timePast
+        }   else if (this.planet === "Jupiter") {
+            let timePast = ((this.avgEarthYear - this.earthYear) * 11.86)
+            return timePast
+        }   
+    }
 }
